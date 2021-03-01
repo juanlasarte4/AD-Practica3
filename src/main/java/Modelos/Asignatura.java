be9148c4655e0014348bb392d6952feb87020b73
juanlasarte4 <0302jlasarte@e-itaca.es>
2021-03-01 03:12:49 +0100

    0.2

--- src/main/java/Modelos/Asignatura.java
+++ src/main/java/Modelos/Asignatura.java
@@ -8,11 +8,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Asignatura {
-	private Integer codigo;
+	private int codigo;
 	private String nombre;
 	private List<Profesor> profesoresQueLaImparten;
 
-	public Asignatura(Integer codigo, String nombre, List<Profesor> profesoresQueLaImparten) {
+	public Asignatura(int codigo, String nombre, List<Profesor> profesoresQueLaImparten) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 		this.profesoresQueLaImparten = profesoresQueLaImparten;
--- src/main/java/Modelos/Profesor.java
+++ src/main/java/Modelos/Profesor.java
@@ -6,13 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Profesor {
-	private Integer codigo;
+	private int codigo;
 	private String nombre;
 	private String apellidos;
 	private String sexo;
 	private String fechaNacimiento;
 
-	public Profesor(Integer codigo, String nombre, String apellidos, String sexo, String fechaNacimiento) {
+	public Profesor(int codigo, String nombre, String apellidos, String sexo, String fechaNacimiento) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
